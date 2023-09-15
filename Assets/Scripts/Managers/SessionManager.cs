3ce4e9abeb446996c7401bf444e6c957646a66b2
Akın Yıldırım <aknyldrm361@gmail.com>
2023-09-15 20:39:01 +0300

    session manager

---
+++ Assets/Scripts/Managers/SessionManager.cs
@@ -0,0 +1,45 @@
+using Fusion;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SessionManager : NetworkBehaviour
+{
+    [SerializeField] private float sessionTime = 60f;
+    [Networked] public TickTimer SessionTimer {get; set;}
+    
+    public static event UnityAction OnSessionStart;
+    public static event UnityAction OnSessionFinish;
+
+    private bool isSessionActive = false;
+
+    public override void Spawned()
+    {
+        StartSession();
+    }
+
+    private void StartSession()
+    {
+        SessionTimer = TickTimer.CreateFromSeconds(Runner, sessionTime);
+        OnSessionStart?.Invoke();
+        isSessionActive = true;
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if(SessionTimer.Expired(Runner) && isSessionActive)
+        {
+            Debug.LogError("Session has ended");
+            OnSessionFinish?.Invoke();
+            isSessionActive = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if(!Runner)
+            return;
+        
+        int remainingTime = (int)SessionTimer.RemainingTime(Runner);
+        GUI.Label(new Rect(0, 0, 100, 100), remainingTime.ToString());
+    }
+}
